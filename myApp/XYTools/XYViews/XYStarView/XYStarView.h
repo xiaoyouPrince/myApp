57151283f7a747d32c8d0f0f3131f8ae53eb224f
xiaoyouPrince <xiaoyouPrince@163.com>
2021-02-17 16:14:09 +0800

    重构XYStarView

--- myApp/XYTools/XYViews/XYStarView/XYStarView.h
+++ myApp/XYTools/XYViews/XYStarView/XYStarView.h
@@ -6,45 +6,52 @@
 //  Copyright © 2018年 zhuang chaoxiao. All rights reserved.
 //
 
-/**
- 使用方式 :
-    如果XIB加载，请在 viewDidAppear 方法中设置starView的frame
+// 1. 核心功能 - 属性值 + 功能框架的抽取
+// 2. 附加优化细节
+
+/*
+    一个星星视图, 可自定义星星图片、星星大小(正方形)、星星间距、
+ */
+
+/*
+ starView 布局规则 frame 布局 / autoLayout
  
- /// 1.设置星星 ,viewDidLoad 中 frame 不准
-     XYStarView *starView = [[XYStarView alloc] initWithFrame:superView.bounds];
-     starView.starCount = 2;
-     [superView addSubview:starView];
+ 1. 提供两种构建方法,直接构建是 autoLayout 布局的。另提供一种 frame 布局的构建方案
  
- /// 2.取start数量
-     CGFloat startCount = starView.starCount;
+ 2. 星星在中间，第一个星的 left.top.bottom 均与 starView 相同。 从第二颗星星开始有星星间的间距。最后一颗星 .right 与 starView 右边距相同。
  
- /// 3.自定义starSize大小,并自动重置星星间距
-     XYStarView *starView = [[XYStarView alloc] initWithFrame:superView.bounds starSize:size autoResizeStarMargin:YES];
-     starView.starCount = 2;
-     [superView addSubview:starView];
+ starView 布局示意图:
  
- /// 4.自定义star图片和星星大小
-     XYStarView *starView = [[XYStarView alloc] initWithFrame:superView.bounds starSize:size autoResizeStarMargin:YES];
-     [starView count:count withName:name]
-     [superView addSubview:starView];
+ ---------------
+ |*-*-*-*-*-*-*|
+ —--------------
  
- /// 5.评价只需设置星星数量为 0
-     XYStarView *starView = [[XYStarView alloc] initWithFrame:superView.bounds autoResizeStarMargin:YES];
-     starView.starCount = 0;  // 当starCount == 0 的时候默认是可评价状态
-     [superView addSubview:starView];
  */
 
 #import <UIKit/UIKit.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface XYStarView : UIView
 
-/** 默认星星，直接设置星星数就行*/
+/** 选中星星数量 default is 34*/
+@property(nonatomic, assign) CGFloat starWH;
+/** 选中星星数量 default is 22*/
+@property(nonatomic, assign) CGFloat starMargin;
+/** 星星总数量 default is 5*/
 @property(nonatomic, assign) NSInteger starCount;
+/** 是否支持交互 default is NO, 如果可以选择星星请手动设置为YES*/
+@property(nonatomic, assign) BOOL allowUserAction;
+/** 选中星星数量 default is 0, 此值会实时更新*/
+@property(nonatomic, assign) NSInteger selectedStarCount;
+/** 星星图片的名称 default 为默认值*/
+@property(nonatomic, copy) NSString* starImageName;
+/** 选择星星回调 */
+@property (nonatomic, copy) void (^starTapHandler)(NSInteger starCount);
 
-/** 自定义的背景图的星星数量*/
-- (void)count:(NSInteger)count withName:(NSString *)name;
++ (instancetype)starViewWithConfig:(nullable void(^)(XYStarView * starView))config;
++ (instancetype)instanceWithConfig:(nullable void(^)(XYStarView * starView))config;
 
-/** 可以自定义星星大小 */
-- (instancetype)initWithFrame:(CGRect)frame starSize:(CGFloat)size autoResizeStarMargin:(BOOL)autoResizeMargin;
+NS_ASSUME_NONNULL_END
 
 @end
--- myApp/XYTools/XYViews/XYStarView/XYStarView.m
+++ myApp/XYTools/XYViews/XYStarView/XYStarView.m
@@ -10,14 +10,10 @@
 
 @implementation XYStarView
 
-static CGFloat _starWH = 34;     // 默认 34pt 宽高
-static CGFloat _starMargin = 22; // 默认是 22pt 间距
-
 - (instancetype)initWithCoder:(NSCoder *)aDecoder
 {
     if (self = [super initWithCoder:aDecoder]) {
-        // 这个默认是星星，放到 设置星星的个数去执行星星背景了。
-        [self setupDefaultSizeAndMargin];
+        [self setupDefault];
     }
     return self;
 }
@@ -25,8 +21,7 @@ static CGFloat _starMargin = 22; // 默认是 22pt 间距
 - (instancetype)initWithFrame:(CGRect)frame
 {
     if (self = [super initWithFrame:frame]) {
-        // 这个默认是星星，放到 设置星星的个数去执行星星背景了。
-        [self setupDefaultSizeAndMargin];
+        [self setupDefault];
     }
     return self;
 }
@@ -34,155 +29,94 @@ static CGFloat _starMargin = 22; // 默认是 22pt 间距
 - (instancetype)init
 {
     if (self = [super init]) {
-        // 这个默认是星星，放到 设置星星的个数去执行星星背景了。
-        [self setupDefaultSizeAndMargin];
+        [self setupDefault];
     }
     return self;
 }
 
-- (void)setupDefaultSizeAndMargin{
-    
-    _starWH = 34;
-    _starMargin = 22;
+- (void)setupDefault{
+    self.starWH = 34;
+    self.starMargin = 22;
+    self.starCount = 5;
+    self.allowUserAction = NO;
+    self.selectedStarCount = 0;
+    self.starImageName = @"icon_star";
 }
 
-- (instancetype)initWithFrame:(CGRect)frame starSize:(CGFloat)size autoResizeStarMargin:(BOOL)autoResizeMargin
+- (void)setStarImageName:(NSString *)starImageName
 {
-    if (self = [super initWithFrame:frame]) {
-        // 这个默认是星星，放到 设置星星的个数去执行星星背景了。
-        _starWH = size; //default is 34
-        
-        if (autoResizeMargin) {
-            //比例关系入下: 34 : 22  = _starWH : _starMargin
-            _starMargin = (_starWH * 22)/34;
-        }else
-        {
-            _starMargin = 22;
-        }
+    UIImage *image = [UIImage imageNamed:starImageName];
+    if (!image) {
+        _starImageName = @"icon_star";
+    }else
+    {
+        _starImageName = starImageName;
     }
-    return self;
 }
 
-
-
-- (void)layoutSubviews{
+- (void)setupStar{
     
-}
-
-/// 设置这里的时候已经有了frame了
-- (void)setBgWithName:(NSString *)name
-{
     // 根据用户设置的size取值
-    CGFloat H = _starWH ? _starWH : 34;
-    CGFloat W = H;
-    
-    NSString *bgNname = [NSString stringWithFormat:@"%@_bg",name];
-    
-    for (int i = 0; i < 5; i++) {
-        UIImageView *iv = [[UIImageView alloc] init];
-        
-        iv.frame = CGRectMake(0, 0, W, H);
-        iv.center = self.center;
-        if (i == 2) { // 中间星星就在中间
-            // 无需操作
-        }else
-        {
-            int index = i - 2;  // 其他的星星以 中间这个为准布局,左右倍数
-            CGFloat deltaX = W + _starMargin;  // 每个星星偏差距离 星星宽度 + 22pt
-            
-            iv.xy_centerX = self.xy_centerX + (index * deltaX);
-        }
-        
-        iv.image = [UIImage imageNamed:bgNname];
-        iv.contentMode = UIViewContentModeScaleAspectFit;
-        [self addSubview:iv];
-    }
-    
-}
-
-
-- (void)setFgWithName:(NSString *)name
-{
-    // 根据用户设置的size取值
-    CGFloat H = _starWH ? _starWH : 34;
+    CGFloat H = _starWH;
     CGFloat W = H;
     
     for (int i = 0; i < _starCount; i++) {
         UIImageView *iv = [[UIImageView alloc] init];
-        
-        iv.frame = CGRectMake(0, 0, W, H);
-        iv.center = self.center;
-        if (i == 2) { // 中间星星就在中间
-            // 无需操作
-        }else
-        {
-            int index = i - 2;  // 其他的星星以 中间这个为准布局,左右倍数
-            CGFloat deltaX = W + _starMargin;  // 每个星星偏差距离 星星宽度 + 22pt
-            iv.xy_centerX = self.xy_centerX + (index * deltaX);
+        CGFloat X = i * (W + _starMargin);
+        iv.frame = CGRectMake( X, 0, W, H);
+        if (i < self.selectedStarCount) {
+            iv.image = [UIImage imageNamed:self.starImageName];
+        }else{
+            NSString *bgNname = [NSString stringWithFormat:@"%@_bg",self.starImageName];
+            iv.image = [UIImage imageNamed:bgNname];
         }
         
-        iv.image = [UIImage imageNamed:name];
         iv.contentMode = UIViewContentModeScaleAspectFit;
+        iv.tag = i;
         [self addSubview:iv];
-    }
-    
-}
-
-- (void)setStarCount:(NSInteger)starCount
-{
-    _starCount = starCount;
-    // 1. 设置背景
-    [self setBgWithName:@"icon_star"];
-    
-    // 2. 设置前景
-    [self setFgWithName:@"icon_star"];
-    
-    // 如果是 0 颗星，就是要评价的状态，
-    if(starCount == 0)
-    {
-        [self.subviews enumerateObjectsUsingBlock:^(__kindof UIImageView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            
-            obj.userInteractionEnabled = YES;
+        
+        if (self.allowUserAction) {
+            iv.userInteractionEnabled = YES;
             UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapStarForAppraise:)];
-            [obj addGestureRecognizer:tap];
-        }];
+            [iv addGestureRecognizer:tap];
+        }
     }
 }
 
 - (void)tapStarForAppraise:(UITapGestureRecognizer *)tap{
     // 设置用户点击星星数量
     UIImageView *iv = (UIImageView *)tap.view;
-    NSUInteger idx = [self.subviews indexOfObject:iv];
-    _starCount = idx + 1; // 供外界使用用户评价了几个星
-    
-    // 选中部分替换成黄色星星，后面不变
+    // 选中部分替换成选中星星，后面不变
     [self.subviews enumerateObjectsUsingBlock:^(__kindof UIImageView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        if (iv == obj) {
-            for (int i = 0; i < idx + 1; i++) {
-                UIImageView *starView = (UIImageView *)self.subviews[i];
-                starView.image = [UIImage imageNamed:@"icon_star"];
-            }
-            
-            for (NSUInteger i = idx + 1; i < 5; i++) {
-                UIImageView *starView = (UIImageView *)self.subviews[i];
-                starView.image = [UIImage imageNamed:@"icon_star_bg"];
-            }
+        
+        NSUInteger index = idx;
+        if (index >= 0 && index <= iv.tag) {
+            obj.image = [UIImage imageNamed:self.starImageName];
+        }else if (index >iv.tag){
+            NSString *bgNname = [NSString stringWithFormat:@"%@_bg",self.starImageName];
+            obj.image = [UIImage imageNamed:bgNname];
         }
     }];
-}
-
-
-// 用户自己设置的，
-- (void)count:(NSInteger)count withName:(NSString *)name
-{
-    _starCount = count;
-    // 1. 设置背景
-    [self setBgWithName:name];
     
-    // 2. 设置前景
-    [self setFgWithName:name];
+    // 通知外界
+    self.selectedStarCount = iv.tag + 1;
+    if (self.starTapHandler) {
+        self.starTapHandler(self.selectedStarCount);
+    }
 }
 
-
++ (instancetype)starViewWithConfig:(void (^)(XYStarView *))config {
+    return [self instanceWithConfig:config];
+}
++ (instancetype)instanceWithConfig:(void (^)(XYStarView *))config {
+    XYStarView *starView = [XYStarView new];
+    if (config) {
+        config(starView);
+    }
+    [starView setupStar];
+    CGFloat width = starView->_starCount * (starView.starWH + starView.starMargin) - starView.starMargin;
+    starView.frame = CGRectMake(0, 0, width, starView.starWH);
+    return starView;
+}
 
 @end
